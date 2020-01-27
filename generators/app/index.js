43c4a84b5b576ffeb51bd3cf5bd0f58cad4f74c9
mansur-hamidov-pb <mansur.hamidov@pashabank.az>
2020-01-28 00:45:27 +0400

    updates

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,6 +10,10 @@ module.exports = class extends Generator {
                 type: "input",
                 name: "appName",
                 message: "Please, enter your app name:"
+            }, {
+                type: "input",
+                name: "appDescription",
+                message: "Please, enter your app description"
             }
         ];
 
@@ -18,11 +22,24 @@ module.exports = class extends Generator {
     }
 
     writing() {
+        const pkgJson = {
+            "name": this.props.appName,
+            "description": this.props.appDescription,
+            "version": "1.0.0",
+            "license": "MIT",
+            "scripts": {
+                "start": "parcel public/index.html --open",
+                "build": "rm -R -f dist .cache && parcel build public/index.html --no-source-maps"
+            }
+        }
+
         this.fs.copyTpl(
             this.templatePath("**/*"),
             this.destinationPath(this.props.appName),
             this.props
         );
+
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
     }
 
     install () {
