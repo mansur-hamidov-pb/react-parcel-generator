0d58b125a8655f8de9f345c369ee649cbfe9716b
Mansur Hamidov <mhamidov@Mansur-Hamidov.local>
2020-01-28 10:57:06 +0400

    Line indentation

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,46 +4,42 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
-    async prompting () {
-        const prompts = [
-            {
-                type: "input",
-                name: "appName",
-                message: "Please, enter your app name:"
-            }, {
-                type: "input",
-                name: "appDescription",
-                message: "Please, enter your app description"
-            }
-        ];
+	async prompting() {
+		const prompts = [
+			{
+				type: "input",
+				name: "appName",
+				message: "Please, enter your app name:"
+			}, {
+				type: "input",
+				name: "appDescription",
+				message: "Please, enter your app description"
+			}
+		];
 
-        const props = await this.prompt(prompts);
-        this.props = props;
-    }
+		const props = await this.prompt(prompts);
+		this.props = props;
+	}
 
-    writing() {
-        this.fs.copyTpl(
-            this.templatePath("**/*"),
-            this.destinationPath(this.props.appName),
-            this.props
-        );
-    }
+	writing() {
+		this.fs.copyTpl(
+			this.templatePath("**/*"),
+			this.destinationPath(this.props.appName),
+			this.props
+		);
+	}
 
-    install () {
-        process.chdir(process.cwd() + '/' + this.props.appName);
+	install() {
+		process.chdir(process.cwd() + '/' + this.props.appName);
 
-        this.npmInstall(
-            [
-                'react',
-                'react-dom'
-            ]
-        );
-        this.npmInstall(
-            [
-                'typescript',
-                '@types/react',
-                '@types/react-dom'
-            ]
-        )
-    }
+		this.npmInstall([
+			'react',
+			'react-dom'
+		]);
+		this.npmInstall([
+			'typescript',
+			'@types/react',
+			'@types/react-dom'
+		])
+	}
 }
